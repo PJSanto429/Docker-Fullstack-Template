10665c5106bab8668a8aed886849a745834cce04
Peyton Santo <peyton.santo429@gmail.com>
2025-10-13 16:57:14 -0400

    full rework of dockerizing and mongo. now it should all be very simple to update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,29 @@
+import { useCallback, useEffect, useState } from "react"
 import { exampleGetRequest } from "./requests/example"
 
 export const App = () => {
 
-    const testThing = async () => {
+    const [text, setText] = useState('')
+
+    const testThing = useCallback(async () => {
         try {
             const data = await exampleGetRequest()
-            alert('Success: ' + data?.message)
+            setText(text + data.message)
         } catch (err) {
             console.error(err)
         }
-    }
+    }, [text])
+
+    useEffect(() => {
+        testThing()
+    }, [])
+
 
     return (
-        <>
-            <button onClick={testThing}>Press me</button>
-        </>
+        <div style={{
+            backgroundColor: 'tan'
+        }}>
+            <>{text}</>
+        </div>
     )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,28 +2,27 @@ import express, { Request, Response } from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-import { GoogleGenAI } from "@google/genai"
 import bodyParser from 'body-parser'
 
 const { Schema, model } = mongoose
 
-// const Thing = model(
-//     'Thing',
-//     new Schema({
-//         name: {
-//             type: String,
-//             required: true
-//         },
-//         age: {
-//             type: Number,
-//             required: true,
-//             default: 50
-//         }
-//     }, {
-//         versionKey: false,
-//         timestamps: true
-//     })
-// )
+const Thing = model(
+    'Thing',
+    new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true,
+            default: 50
+        }
+    }, {
+        versionKey: false,
+        timestamps: true
+    })
+)
 
 const app = express()
 
@@ -35,10 +34,13 @@ app.use(cors({
     credentials: true
 }))
 
-// const ai = new GoogleGenAI({ apiKey: 'AIzaSyDV0gSqF_jY27ApkWIZ0f2aqAO7G-rdtr4' })
 
 app.get('/', async (req: Request, res: Response) => {
-    res.status(200).json({message: 'Greetings from the server!'})
+    const items = await Thing.find({})
+    res.status(200).json({
+        message: 'Hello from the server! Here are the items:',
+        data: items
+    })   
 })
 
 // app.delete('/', async (_req: Request, res: Response) => {
@@ -49,18 +51,6 @@ app.get('/', async (req: Request, res: Response) => {
 // app.post('/', async (req: Request, res: Response) => {
 //     const created = await Thing.create(req.body)
 //     res.status(200).json(created)
-// })
-
-// app.put('/ai', async (req: Request, res: Response) => {
-//     const { prompt } = req.body
-    
-//     const response = await ai.models.generateContent({
-//         model: "gemini-2.0-flash",
-//         contents: prompt
-//     })
-//     res.status(200).json({
-//         response: response.text
-//     })
 // })
 
 // app.put('/:thingId', async (req: Request, res: Response) => {
@@ -83,8 +73,8 @@ app.get('/', async (req: Request, res: Response) => {
 
 const start = async () => {
     try {
-        const port = 5678
-        await mongoose.connect('mongodb://localhost:1919/mydb')
+        const port = 5000
+        await mongoose.connect('mongodb://mongo:27017/mydb')
         console.log('Connected to MongoDB')
         app.listen(port, () => console.log('Server listening on port ' +  port +'\n\n\n\n\n\n\n\n'))
     } catch (err) {
